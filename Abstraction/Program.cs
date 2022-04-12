2d2bec9ef2628ee9c6eb5e206f2d8168e21212c2
Serkan Topkan <stopkan@isbak.istanbul>
2022-04-12 08:29:30 +0300

    Abstraction eklendi

---
+++ Abstraction/Program.cs
@@ -0,0 +1,38 @@
+﻿// Abstract Class : Abstract class’lardan instance object (örnek nesne) oluşturulamaz. Abstract class’a ulaşmak için başka bir sınıftan kalıtım alınması gerekir.
+// Abstract Method : Sadece abstract class içerisinde kullanılır. Metot içerisine herhangi birşey yazılmaz. Bir abstract class hem abstract metotlara hem de normal metotlara sahip olabilir. Bu kısım önemli. Şimdi bir örnekle bunu gösterelim.
+
+namespace HelloWorld
+{
+
+    public abstract class Vehicle
+    {
+        public abstract double GetFuelInfo();
+    }
+
+    public class Car : Vehicle
+    {
+        public Car()
+        {
+
+        }
+
+        public override double GetFuelInfo()
+        {
+            return 15;
+        }
+    }
+
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car bmw = new Car();
+            var FuelInfo = bmw.GetFuelInfo();
+            Console.WriteLine("BMW Fuel Info:{0}", FuelInfo.ToString());
+
+        }
+    }
+}
