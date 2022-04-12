4344a606dda3b2bb31f9c65bd8f0026f1c101928
Serkan Topkan <stopkan@isbak.istanbul>
2022-04-12 08:17:10 +0300

    Encapsulation kodları eklendi

--- Encapsulation/Program.cs
+++ Encapsulation/Program.cs
@@ -1,2 +1,63 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Parent
+{
+    public class A
+    {
+        public A()
+        {
+            //B erişim yok
+            C c = new C();//Erişim var
+        }
+
+        protected void T()
+        {
+
+        }
+
+    }
+
+    public class E : A
+    {
+        public E()
+        {
+            T();//protected imzalı methoda kalıtım alındığı için erişim var
+        }
+    }
+
+    private class B
+    {
+        public B()
+        {
+            A a = new A();//Erişim var
+            //A.T Erişim Yok
+            C c = new C();//Erişim var     
+        }
+    }
+
+    internal class C
+    {
+        //B Erişim yok
+        public C()
+        {
+            A a = new A();//Erişim var
+            //a.T Erişim yok
+
+
+        }
+    }
+}
+
+namespace Child
+{
+    using Parent;
+    public class D
+    {
+        public D()
+        {
+            A a = new A();//Erişim var
+                          //B Erişim yok
+            C c = new C();//Erişim var
+            //a.T Erişim yok
+        }
+
+    }
+}
