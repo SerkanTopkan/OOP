904a23aef170b789744f42f4dd5b63cf2b596a1a
Serkan Topkan <stopkan@isbak.istanbul>
2022-04-12 08:44:22 +0300

    Polymorphism eklendi

---
+++ Polymorphism/Program.cs
@@ -0,0 +1,26 @@
+﻿public class Calc
+{
+
+    public int DateOfBirthCalc(DateTime date)
+    {
+        return DateTime.Now.Year - date.Year;
+    }
+
+    public int DateOfBirthCalc(int yearOfBirth)
+    {
+        return DateTime.Now.Year - yearOfBirth;
+    }
+}
+class Program
+{
+
+    static void Main(string[] args)
+    {
+        Calc c = new Calc();
+        var result = c.DateOfBirthCalc(DateTime.Now.AddYears(-42));
+        var result1 = c.DateOfBirthCalc(42);
+
+        Console.WriteLine("Result Int : {0} - Result Year : {1}", result, result1);
+
+    }
+}
